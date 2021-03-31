3bcd0d4cf6037d762ab329baa871b37d70ce7e65
Heloy <heloy.leal@hotmail.com>
2021-03-31 09:50:38 +0200

    upvotes fetched from server

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import ArticleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
+
 const ArticlesPage = ({ match }) => {
     const name = match.params.name;
     const article = ArticleContent.find(article =>
         article.name === name);
+    //react hooks
+    const [articleInfo,setArticleInfo] = useState({upvotes:0,comments:[]});
+    useEffect(()=>{
+        const fetchData = async () =>{
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+        
+    },[name]);
+
     if (!article) return <NotFoundPage />
     const otherArticles = ArticleContent.filter(article => article.name !==name);    
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This post has been upvoted {articleInfo.upvotes} times</p>
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
